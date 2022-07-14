776f77835c1bb3f05f493e49d3eb0947b8e47f44
Igor Lourenço <igorlourenco.dev@gmail.com>
2022-07-14 10:54:53 -0300

    feat: add licenses

--- interface/IPollenNft.sol
+++ interface/IPollenNft.sol
@@ -1,14 +1,12 @@
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 interface IPollenNft {
+    function createToken(address sender) external returns (uint256 id);
 
-function createToken(address sender) external returns (uint256 id);
+    function balanceOf(address sender) external view returns (uint256 id);
 
-function balanceOf(address sender) external view returns (uint256 id); 
+    function ownerOf(uint256 tokenId) external view returns (address sender);
 
-function ownerOf(uint256 tokenId) external view returns ( address sender);
-
-function burn(uint256 tokenId) external;
-   
-
+    function burn(uint256 tokenId) external;
 }
